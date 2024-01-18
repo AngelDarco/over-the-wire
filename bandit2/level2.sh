09eba43f8537a620ecad0d061752371558f569b8
Angel Darco <angel92-eme@hotmail.com>
2024-01-18 12:47:04 -0500

    :sparkles: level2 solved

---
+++ bandit2/level2.sh
@@ -0,0 +1,22 @@
+#                         _                     _ _ _   
+#                        | |__   __ _ _ __   __| (_) |_ 
+#                        | '_ \ / _` | '_ \ / _` | | __|
+#                        | |_) | (_| | | | | (_| | | |_ 
+#                        |_.__/ \__,_|_| |_|\__,_|_|\__|
+#                                                       
+#
+#                      This is an OverTheWire game server. 
+#            More information on http://www.overthewire.org/wargames
+#
+#
+# SSH
+ ssh bandit2@bandit.labs.overthewire.org -p 2220
+
+# Password: 
+#rRGizSaX8Mk1RTb1CNQoXTcYZWU6lgzi
+
+# Code
+# cat *filename
+
+# flag
+# aBZ0W5EmUfAf7kHTQeOwd8bauFJ2lAiG
