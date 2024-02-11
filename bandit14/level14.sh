b4e990d35ede5175615a8649238a2500e7861b09
Angel Darco <angel92-eme@hotmail.com>
2024-02-10 23:59:31 -0500

    :racehorse: bandit14 solved

---
+++ bandit14/level14.sh
@@ -0,0 +1,22 @@
+#                         _                     _ _ _   
+#                        | |__   __ _ _ __   __| (_) |_ 
+#                        | '_ \ / _` | '_ \ / _` | | __|
+#                        | |_) | (_| | | | | (_| | | |_ 
+#                        |_.__/ \__,_|_| |_|\__,_|_|\__|
+#                                                       
+#
+#                      This is an OverTheWire game server. 
+#            More information on http://www.overthewire.org/wargames
+#
+#
+# SSH
+ssh bandit14@bandit.labs.overthewire.org -p 2220
+
+# Password:
+# fGrHPx402xGC7U7rXKDaxiWFTOiF0ENq 
+
+# Code: 
+# 
+
+# Flag:
+# 
