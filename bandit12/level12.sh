b837d61474b663bc23865c0ec9d8c6201e123197
Angel Darco <angel92-eme@hotmail.com>
2024-02-10 23:59:07 -0500

    :racehorse: bandit12 solved

---
+++ bandit12/level12.sh
@@ -0,0 +1,23 @@
+#                         _                     _ _ _   
+#                        | |__   __ _ _ __   __| (_) |_ 
+#                        | '_ \ / _` | '_ \ / _` | | __|
+#                        | |_) | (_| | | | | (_| | | |_ 
+#                        |_.__/ \__,_|_| |_|\__,_|_|\__|
+#                                                       
+#
+#                      This is an OverTheWire game server. 
+#            More information on http://www.overthewire.org/wargames
+#
+#
+# SSH
+ssh bandit12@bandit.labs.overthewire.org -p 2220
+
+# Password:
+# JVNBBFSmZwKKOP0XbFXOoW8chDz5yVRv
+
+# Code: 
+# bash decompresor.sh
+ 
+
+# Flag:
+# wbWdlBxEir4CaE8LaPhauuOo6pwRmrDw
