254677b875a972aaf545eebe6c58d2d3bd3f16a3
Angel Darco <angel92-eme@hotmail.com>
2025-01-04 13:37:15 -0500

    :sparkles: bandit24 solved

---
+++ bandit24/level24.sh
@@ -0,0 +1,23 @@
+#                         _                     _ _ _
+#                        | |__   __ _ _ __   __| (_) |_
+#                        | '_ \ / _` | '_ \ / _` | | __|
+#                        | |_) | (_| | | | | (_| | | |_
+#                        |_.__/ \__,_|_| |_|\__,_|_|\__|
+#
+#
+#                      This is an OverTheWire game server.
+#            More information on http://www.overthewire.org/wargames
+#
+
+#
+# SSH
+ssh bandit24@bandit.labs.overthewire.org -p 2220
+
+# Password:
+# gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8
+
+# Code:
+#
+
+# Flag:
+#
