56dc4b815050a5365965a1aa546c40ade0ce4307
Angel Darco <angel92-eme@hotmail.com>
2025-01-05 11:26:40 -0500

    :sparkles: bandit32 solved

---
+++ bandit32/level32.sh
@@ -0,0 +1,22 @@
+#                         _                     _ _ _
+#                        | |__   __ _ _ __   __| (_) |_
+#                        | '_ \ / _` | '_ \ / _` | | __|
+#                        | |_) | (_| | | | | (_| | | |_
+#                        |_.__/ \__,_|_| |_|\__,_|_|\__|
+#
+#
+# This is an OverTheWire game server.
+# More information on http://www.overthewire.org/wargames
+#
+# SSH
+ssh bandit32@bandit.labs.overthewire.org -p 2220
+
+# Password:
+# 3O9RfhqyAlVBEZpVb6LYStshZoqoSx5K
+
+# code:
+# $0
+# cat /etc/bandit_pass/bandit33
+
+# flag
+# tQdtbs5D5i2vJwkO8mEyYEyTL8izoeJ0
