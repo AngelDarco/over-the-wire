e626d043276a29abb242aae1bee3f225e667cef8
Angel Darco <angel92-eme@hotmail.com>
2025-01-05 11:25:46 -0500

    :sparkles: bandit27 solved

---
+++ bandit27/level27.sh
@@ -0,0 +1,24 @@
+#                         _                     _ _ _
+#                        | |__   __ _ _ __   __| (_) |_
+#                        | '_ \ / _` | '_ \ / _` | | __|
+#                        | |_) | (_| | | | | (_| | | |_
+#                        |_.__/ \__,_|_| |_|\__,_|_|\__|
+#
+#
+# This is an OverTheWire game server.
+# More information on http://www.overthewire.org/wargames
+#
+# SSH
+ssh bandit27@bandit.labs.overthewire.org -p 2220
+
+# Password:
+# upsNCc7vzaRDx6oZC6GiR6ERwe1MowGB
+
+# code:
+# mktemp -d
+# git clone ssh://bandit27-git@localhost:2220/home/bandit27-git/repo
+# cd repo
+# cat README
+
+# flag
+# Yz9IpL0sBcCeuG7m9uQFt8ZNpS4HZRcN
