ab3298423e96f9914427ebd5aa40c32ca874843e
Angel Darco <angel92-eme@hotmail.com>
2025-01-05 11:26:31 -0500

    :sparkles: bandit31 solved

---
+++ bandit31/level31.sh
@@ -0,0 +1,28 @@
+#                         _                     _ _ _
+#                        | |__   __ _ _ __   __| (_) |_
+#                        | '_ \ / _` | '_ \ / _` | | __|
+#                        | |_) | (_| | | | | (_| | | |_
+#                        |_.__/ \__,_|_| |_|\__,_|_|\__|
+#
+#
+# This is an OverTheWire game server.
+# More information on http://www.overthewire.org/wargames
+#
+# SSH
+ssh bandit31@bandit.labs.overthewire.org -p 2220
+
+# Password:
+# fb5S2xb7bRyFmAvQYQGEqsbhVyJqhnDy
+
+# code:
+# mktemp -d
+# git clone ssh://bandit31-git@localhost:2220/home/bandit31-git/repo
+# cd repo
+# rm -r .gitignore
+# echo "May I come in?" >> key.txt
+# git add key.txt
+# git commit -m "commit message"
+# git push origin master
+
+# flag
+# 3O9RfhqyAlVBEZpVb6LYStshZoqoSx5K
