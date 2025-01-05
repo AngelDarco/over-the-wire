248f94964e630a2df02c6e4024906c9abda86853
Angel Darco <angel92-eme@hotmail.com>
2025-01-05 11:24:55 -0500

    :sparkles: bandit25 solved

--- bandit25/level25.sh
+++ bandit25/level25.sh
@@ -17,7 +17,15 @@ ssh bandit25@bandit.labs.overthewire.org -p 2220
 # iCi86ttT4KSNe1armKiwbQNmB3YJP3q4
 
 # Code:
-#
+# to show the current terminal
+# cat /etc/passwd | grep bandit26
+# cat /usr/bin/showtext
+
+# (!Important) On a minimized version of 2 lines terminal, run the ssh command to watch the: more command
+# ssh -i bandit26.sshkey bandit26@localhost -p 2220
+# press 'v' to have a exec command line in the more command
+# :set shell=/bin/bash
+# :shell
 
 # Flag:
-#
+# s0773xxkk0MXfdqOfPRVr9L3jJBUOgCZ
