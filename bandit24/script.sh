1655c2d57c49ecbf4fda697d23d163c2a0119472
Angel Darco <angel92-eme@hotmail.com>
2025-01-09 00:20:38 -0500

    :fire: files reestructured to new location

--- bandit24/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## create a dicctionary with all the possible passwords
-PASS="gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8"
-
-for i in {0000..9999}; do
-    echo $PASS $i >>dictionary.txt
-done
