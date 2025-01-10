13629727089c6506b55252f9e8eafdd9224a4dbc
Angel Darco <angel92-eme@hotmail.com>
2025-01-10 15:03:03 -0500

    :sparkles: natas 2 solved

---
+++ natas/natas2/level2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Username: natas2
+Password="TguMNxKo1DSa1tujBLuZJnDUlCcUAPlI"
+
+# URL:
+curl -s -u natas2:$Password http://natas2.natas.labs.overthewire.org/
+
+# Code:
+# Escan in the pixel.png folder
+# URL/files/
+
+# retrieve the flag from user.txt
+# curl -s -u natas2:TguMNxKo1DSa1tujBLuZJnDUlCcUAPlI http://natas2.natas.labs.overthewire.org/files/users.txt -o ./flag.txt
+
+# Flag:
+# 3gqisGdR0pjm6tpkDKdIWO2hSvchLeYH
