e9fa70fb9174a3501779a5015e963afbef90ad73
Angel Darco <angel92-eme@hotmail.com>
2025-01-10 15:02:55 -0500

    :sparkles: natas 1 solved

---
+++ natas/natas1/level1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Username: natas1
+Password="0nzCigAq7t2iALyvU9xcHlYN4MlkIwlq"
+
+# URL:
+curl -s -u natas1:$Password http://natas1.natas.labs.overthewire.org
+
+# Code:
+# Ctrl+i
+# Elements tab
+
+# Flag:
+# TguMNxKo1DSa1tujBLuZJnDUlCcUAPlI
+
