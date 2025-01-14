8ed244928739a8cab62c62e66c438d104f164dea
Angel Darco <angel92-eme@hotmail.com>
2025-01-14 13:48:58 -0500

    :sparkles: natas 16 solved

---
+++ natas/natas16/level16.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ Username="natas16"
+ Password="hPkjKYviLQctEW33QmuXL6eDVfMW4sGo"
+
+# URL:
+# curl -s -u $Username:$Password http://natas16.natas.labs.overthewire.org
+
+# Code:
+# create a script in python to inject a blind sql query to get the flag
+# run the script.py and get the flag
+
+# Flag:
+# EqjHJbo7LFNb8vwhHb9s75hokh5TF0OC
