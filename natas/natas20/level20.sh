1e2a9c7a625df5549f5520f8762be9549a064d69
Angel Darco <angel92-eme@hotmail.com>
2025-01-16 00:22:46 -0500

    :sparkles: natas 20 solved

---
+++ natas/natas20/level20.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ Username="natas20"
+ Password="p5mCvP7GS2K6Bmt3gqhM2Fc1A5T8MVyw"
+
+# URL:
+# curl -s -u $Username:$Password http://natas20.natas.labs.overthewire.org
+
+# Code:
+# curl -s -c cookie -d "debug=1&name=natas21" -u $Username:$Password http://natas20.natas.labs.overthewire.org?PHPSESSID=dqku25j97girn4us3cau8vg87z
+
+# Flag:
+#
+echo "This is a prompt test: "
+read -e -p "Enter the flag: " 
